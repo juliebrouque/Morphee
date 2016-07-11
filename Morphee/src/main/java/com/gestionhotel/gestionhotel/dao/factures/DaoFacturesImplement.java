ca05b2df1e93dcecd470136c7ddb6a4337236b4f
juliebrouque <julie.brouque@live.fr>
2016-07-11 16:30:20 +0200

    DaoFacture

--- Morphee/src/main/java/com/gestionhotel/gestionhotel/dao/factures/DaoFacturesImplement.java
+++ Morphee/src/main/java/com/gestionhotel/gestionhotel/dao/factures/DaoFacturesImplement.java
@@ -1,5 +1,108 @@
 package com.gestionhotel.gestionhotel.dao.factures;
 
-public class DaoFacturesImplement {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.gestionhotel.gestionhotel.entities.Client;
+import com.gestionhotel.gestionhotel.entities.Consommation;
+import com.gestionhotel.gestionhotel.entities.Factures;
+import com.gestionhotel.gestionhotel.entities.Personnes;
+import com.gestionhotel.gestionhotel.entities.Reservations;
+
+/*
+ * Projet: Morphee
+ * Class: DaoFacturesImplemeny
+ * Date: 11/07/2016
+ * Version: 1.0.0
+ * Author: Julie Brouqué
+ */
+public class DaoFacturesImplement implements IDaoFactures{
+
+	Logger log=Logger.getLogger("DaoFacturesImplement");
+	
+	@PersistenceContext //pour instancier l'entityManager
+	private EntityManager em;
+	
+	//Methodes 
+	
+	
+	@Override
+	public Factures addFactures(Factures f) {
+		em.persist(f);
+		return f;
+	}
+
+	@Override
+	public void addReservationFacture(Long idReservation, Long idFacture) {
+		Reservations r=em.find(Reservations.class, idReservation);
+		Factures f=em.find(Factures.class, idFacture);
+		f.getTabReservationsFactures().add(r);
+		log.info("La réservation "+r.getIdReservation()+" a bien été rajoutée à la facture"+f.getIdFacture());
+		
+	}
+
+	@Override
+	public List<Factures> getFactures(Long idClient) {
+		List<Factures> tabFac=new ArrayList<Factures>();
+		Personnes p=em.find(Client.class, idClient);
+		List<Reservations> tabRes=p.getTabReservationPersonne();
+		for(Reservations r:tabRes){
+			if(r.getPersonne().getIdPersonnes().equals(idClient)){
+				tabFac.add(r.getFacture());
+			}
+		}
+		return tabFac;
+	}
+
+	@Override
+	public double getCoutAnnuelClient(int Année, Long idClient) {
+		Personnes p=em.find(Client.class, idClient);
+		List<Reservations> tabRes=p.getTabReservationPersonne();
+		double coutReservation=0;
+		double coutCons=0;
+		double coutChambre=0;
+		for(Reservations res:tabRes){
+			Calendar c = Calendar.getInstance(); 
+			c.setTime(res.getDateSortie());
+			int year = c.get(Calendar.YEAR);
+			if(Année==year){
+				coutChambre=res.getChambre().getPrixChambre();
+				List<Consommation> tabCons=res.getTabConsommationreservation();
+				for(Consommation cons:tabCons){
+					coutCons=cons.getProduit().getPrixProduit();
+				}
+			}
+		}
+		coutReservation=coutChambre+coutCons;
+		return coutReservation;
+	}
+
+	@Override
+	public double getCoutSemestrielClient(Date dateDebut, Date dateFin, Long idClient) {
+		Personnes p=em.find(Client.class, idClient);
+		List<Reservations> tabRes=p.getTabReservationPersonne();
+		double coutReservation=0;
+		double coutCons=0;
+		double coutChambre=0;
+		for(Reservations res:tabRes){
+			if(dateDebut.before(res.getDateArrivee()) && dateFin.after(res.getDateSortie())){
+				coutChambre=res.getChambre().getPrixChambre();
+				List<Consommation> tabCons=res.getTabConsommationreservation();
+				for(Consommation c:tabCons){
+					coutCons=c.getProduit().getPrixProduit();
+				}
+			}
+		}
+		coutReservation=coutChambre+coutCons;
+		return coutReservation;
+	}
 
 }
--- Morphee/src/main/java/com/gestionhotel/gestionhotel/dao/factures/IDaoFactures.java
+++ Morphee/src/main/java/com/gestionhotel/gestionhotel/dao/factures/IDaoFactures.java
@@ -1,5 +1,23 @@
 package com.gestionhotel.gestionhotel.dao.factures;
+/*
+ * Projet: Morphee
+ * Interface: IDaoFactures
+ * Date: 11/07/2016
+ * Version: 1.0.0
+ * Author: Julie Brouqué
+ */
+import java.util.Date;
+import java.util.List;
 
-public class IDaoFactures {
+import com.gestionhotel.gestionhotel.entities.Factures;
 
+
+
+public interface IDaoFactures {
+
+	public Factures addFactures(Factures f);
+	public void addReservationFacture(Long idReservation, Long idFacture);
+	public List<Factures> getFactures(Long idClient);
+	public double getCoutAnnuelClient(int Année, Long idClient);
+	public double getCoutSemestrielClient(Date dateDebut, Date dateFin, Long idClient);
 }
