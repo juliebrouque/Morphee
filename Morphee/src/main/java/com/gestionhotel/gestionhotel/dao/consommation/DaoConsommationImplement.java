e10d43a5f33af0dd3b6df4ac1de0c10b8472d040
inti0298 <inti0298@DESKTOP-66N0E79>
2016-07-11 17:45:21 +0200

    daoConsommation

--- Morphee/src/main/java/com/gestionhotel/gestionhotel/dao/consommation/DaoConsommationImplement.java
+++ Morphee/src/main/java/com/gestionhotel/gestionhotel/dao/consommation/DaoConsommationImplement.java
@@ -1,5 +1,40 @@
 package com.gestionhotel.gestionhotel.dao.consommation;
 
-public class DaoConsommationImplement {
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+
+import com.gestionhotel.gestionhotel.entities.Consommation;
+import com.gestionhotel.gestionhotel.entities.Produits;
+
+/**
+ * 
+ * @author inti0298
+ *classe : classe qui définit les méthodes de l'interface consommation
+ *version: 1.0.0
+ *date: 11/07/2016
+ */
+public class DaoConsommationImplement implements IDaoConsommation{
+Logger log = Logger.getLogger("DaoConsommationImplement");
+private EntityManager em;
+	@Override
+	public Consommation addConsommation(Consommation c) {
+		em.persist(c);
+		log.info("la consommation" + c.getIdConsommation()+ "a bien été enregistrée");
+		return c;
+	}
+
+	@Override
+	public Consommation addconsommationSachantProduit(Consommation c,
+			Long idProduit) {
+		Produits p = em.find(Produits.class, idProduit);
+		c.setProduit(p);
+		em.persist(c);
+		log.info("la consommation " + c.getIdConsommation()+ 
+				"avec le produit" + p.getIdProduit()+"a bien été enregistrée" );
+		return c;
+	}
+
+	
 
 }
