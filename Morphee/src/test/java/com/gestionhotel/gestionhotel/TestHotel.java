4cea73659bf65670159062bfefb19032b05b22b2
juliebrouque <julie.brouque@live.fr>
2016-07-12 10:01:52 +0200

    Test

---
+++ Morphee/src/test/java/com/gestionhotel/gestionhotel/TestHotel.java
@@ -0,0 +1,55 @@
+package com.gestionhotel.gestionhotel;
+/*
+ * Projet: Morphee
+ * Class: TestHotel
+ * Date: 11/07/2016
+ * Version: 1.0.0
+ * Author: Julie Brouqué
+ */
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.gestionhotel.gestionhotel.entities.Hotel;
+import com.gestionhotel.gestionhotel.metier.hotel.IMetierHotel;
+
+public class TestHotel {
+
+	private static IMetierHotel metier;
+	private static ClassPathXmlApplicationContext context;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		context = new ClassPathXmlApplicationContext("app.xml");
+		metier =  (IMetierHotel) context.getBean("serviceHotel");
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		context.close();
+	}
+	
+	@Test
+	public void testAddHotel() {
+		Hotel h1= new Hotel("Ibis", 3, "4 rue des moines", "75 013", "France", "01 77 56 89 63", "01 77 56 89 75", "hotel-ibis@paris.fr");
+		metier.addHotel(h1);
+		Hotel h2= new Hotel("Ritz", 5, "65 rue de LaFayette", "75 001", "France", "01 95 95 64 03", "01 95 95 64 75", "hotel-ritz@paris.fr");
+		metier.addHotel(h2);
+		Hotel h3= new Hotel("Camp", 5, "65 rue de LaFayette", "75 001", "France", "01 95 95 64 03", "01 95 95 64 75", "hotel-ritz@paris.fr");
+		metier.addHotel(h3);
+	}
+
+	/*@Test
+	public void testGetTabEmploye() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGettabClient() {
+		fail("Not yet implemented");
+	}*/
+
+}
